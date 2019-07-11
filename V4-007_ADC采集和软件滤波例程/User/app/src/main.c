67a5492c80c6aef8a123b9bbd4fc5690e22b3711
Singerwall <1042335226@qq.com>
2019-07-11 09:46:06 +0800

    第一次测试,检测.ignore文件是否正常

---
+++ V4-007_ADC采集和软件滤波例程/User/app/src/main.c
@@ -0,0 +1,271 @@
+/*
+*********************************************************************************************************
+*
+*	模块名称 : 主程序入口
+*	文件名称 : main.c
+*	版    本 : V1.0
+*	说    明 : 串口通信例子，和PC机超级终端软件进行交互
+*	修改记录 :
+*		版本号  日期       作者    说明
+*		V1.0    2015-08-30 armfly  首发
+*
+*	Copyright (C), 2015-2016, 安富莱电子 www.armfly.com
+*
+*********************************************************************************************************
+*/
+
+#include "bsp.h"				/* 底层硬件驱动 */
+
+/* 定义例程名和例程发布日期 */
+#define EXAMPLE_NAME	"V4-006_ADC采集例程"
+#define EXAMPLE_DATE	"2015-08-30"
+#define DEMO_VER		"1.0"
+
+static void PrintfLogo(void);
+static void PrintfHelp(void);
+static uint16_t GetADC(void);
+static void ADC_Configuration(void);
+
+#define SAMP_COUNT	20		/* 样本个数，表示200ms内的采样数据求平均值 */
+
+uint16_t g_usAdcValue;	/* ADC 采样值的平均值 */
+
+/*
+*********************************************************************************************************
+*	函 数 名: main
+*	功能说明: c程序入口
+*	形    参：无
+*	返 回 值: 错误代码(无需处理)
+*********************************************************************************************************
+*/
+int main(void)
+{
+	uint8_t ucKeyCode;	
+
+	/*
+		ST固件库中的启动文件已经执行了 SystemInit() 函数，该函数在 system_stm32f4xx.c 文件，主要功能是
+	配置CPU系统的时钟，内部Flash访问时序，配置FSMC用于外部SRAM
+	*/
+	bsp_Init();		/* 硬件初始化 */
+	
+	PrintfLogo();	/* 打印例程名称和版本等信息 */
+	PrintfHelp();	/* 打印操作提示 */
+
+	ADC_Configuration();	/* 配置PC4 为ADC1_IN14 */
+
+	bsp_StartAutoTimer(1, 300);	/* 定时器0周期 300毫秒 */
+
+	/* 进入主程序循环体 */
+	while (1)
+	{
+		bsp_Idle();		/* CPU空闲时执行的函数，在 bsp.c */
+
+		if (bsp_CheckTimer(1))	/* 定时到 */
+		{
+			uint16_t adc;
+			
+			adc = GetADC();
+
+			/* 注意： 末尾只有 \r回车, 没有\n换行，可以使PC超级终端界面稳定在1行显示 */
+			{
+				/* 超级终端界面上会显示一个不断旋转的字符
+				增加这个功能，是为了避免程序死机的假象，因为ADC采样值很稳定
+				*/
+				static uint8_t pos = 0;
+
+				if (pos == 0)
+					printf("|");
+				else if (pos == 1)
+					printf("/");
+				else if (pos == 2)
+					printf("-");
+				else if (pos == 3)
+					printf("\\");		/* 注意：这个特殊字符需要转义 */
+
+				if (++pos >= 4)
+				{
+					pos = 0;
+				}
+
+				printf(" PC4口线ADC采样值 = %5d , 电压 = %4dmV\r",
+					adc, ((uint32_t)adc * 2500) / 4095);
+
+			}
+		}
+
+		/* 处理按键事件 */
+		ucKeyCode = bsp_GetKey();
+		if (ucKeyCode > 0)
+		{
+			/* 有键按下 */
+			switch (ucKeyCode)
+			{
+				case KEY_DOWN_K1:		
+					break;		
+				
+				case KEY_DOWN_K2:
+					break;	
+					
+				default:
+					break;
+			}
+		}
+	}
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: ADC_Configuration
+*	功能说明: 配置ADC, PC4作为ADC通道输入
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static void ADC_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+	__IO uint16_t ADCConvertedValue;
+
+    /* 使能 ADC1 and GPIOC clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOC, ENABLE);
+
+	/* 配置PC4为模拟输入(ADC Channel14) */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	/* 配置ADC1, 不用DMA, 用软件触发 */
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfChannel = 1;
+	ADC_Init(ADC1, &ADC_InitStructure);
+
+	/* 配置ADC1 规则通道14 channel14 configuration */
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_14, 1, ADC_SampleTime_55Cycles5);
+
+	/* 使能ADC1 DMA功能 */
+	ADC_DMACmd(ADC1, ENABLE);
+
+	/* 使能 ADC1 */
+	ADC_Cmd(ADC1, ENABLE);
+
+	/* 使能ADC1 复位校准寄存器 */
+	ADC_ResetCalibration(ADC1);
+	/* 检查ADC1的复位寄存器 */
+	while(ADC_GetResetCalibrationStatus(ADC1));
+
+	/* 启动ADC1校准 */
+	ADC_StartCalibration(ADC1);
+	/* 检查校准是否结束 */
+	while(ADC_GetCalibrationStatus(ADC1));
+
+	/* 软件启动ADC转换 */
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: AdcPro
+*	功能说明: ADC采样处理，插入1ms systick 中断进行调用
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+void AdcPro(void)
+{
+	static uint16_t buf[SAMP_COUNT];
+	static uint8_t write;
+	uint32_t sum;
+	uint8_t i;
+
+	buf[write] = ADC_GetConversionValue(ADC1);
+	if (++write >= SAMP_COUNT)
+	{
+		write = 0;
+	}
+
+	/* 下面这段代码采用求平均值的方法进行滤波
+		也可以改善下，选择去掉最大和最下2个值，使数据更加精确
+	*/
+	sum = 0;
+	for (i = 0; i < SAMP_COUNT; i++)
+	{
+		sum += buf[i];
+	}
+	g_usAdcValue = sum / SAMP_COUNT;	/* ADC采样值由若干次采样值平均 */
+
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);	/* 软件启动下次ADC转换 */
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: GetADC
+*	功能说明: 读取ADC采样的平均值
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static uint16_t GetADC(void)
+{
+	uint16_t ret;
+
+	/* 因为	g_AdcValue 变量在systick中断中改写，为了避免主程序读变量时被中断程序打乱导致数据错误，因此需要
+	关闭中断进行保护 */
+
+	/* 进行临界区保护，关闭中断 */
+	__set_PRIMASK(1);  /* 关中断 */
+
+	ret = g_usAdcValue;
+
+	__set_PRIMASK(0);  /* 开中断 */
+
+	return ret;
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: PrintfHelp
+*	功能说明: 打印操作提示
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static void PrintfHelp(void)
+{
+	printf("请调节开发板上的精密可调电阻，观察ADC采样值的变化\r\n");
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: PrintfLogo
+*	功能说明: 打印例程名称和例程发布日期, 接上串口线后，打开PC机的超级终端软件可以观察结果
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static void PrintfLogo(void)
+{
+	printf("*************************************************************\n\r");
+	printf("* 例程名称   : %s\r\n", EXAMPLE_NAME);	/* 打印例程名称 */
+	printf("* 例程版本   : %s\r\n", DEMO_VER);		/* 打印例程版本 */
+	printf("* 发布日期   : %s\r\n", EXAMPLE_DATE);	/* 打印例程日期 */
+
+	/* 打印ST固件库版本，宏定义在 stm32f4xx.h 文件 */
+	printf("* 固件库版本 : %d.%d.%d\r\n", __STM32F10X_STDPERIPH_VERSION_MAIN,
+			__STM32F10X_STDPERIPH_VERSION_SUB1,__STM32F10X_STDPERIPH_VERSION_SUB2);
+
+	/* 打印 CMSIS 版本. 宏定义在 core_cm4.h 文件 */
+	printf("* CMSIS版本  : %X.%02X\r\n", __CM3_CMSIS_VERSION_MAIN, __CM3_CMSIS_VERSION_SUB);
+
+	printf("* \n\r");	/* 打印一行空格 */
+	printf("* QQ    : 1295744630 \r\n");
+	printf("* Email : armfly@qq.com \r\n");
+	printf("* Copyright www.armfly.com 安富莱电子\r\n");
+	printf("*************************************************************\n\r");
+}
+
+/***************************** 安富莱电子 www.armfly.com (END OF FILE) *********************************/
